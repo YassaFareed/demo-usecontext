864f7bb42bc6c7f7ff6d465b3dcd207dc0157e4e
Yassa Fareed <yassa.fareed99@gmail.com>
2020-07-23 21:41:42 +0500

    updated usecontext to work with usestate

--- src/ValueContext.js
+++ src/ValueContext.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const ValueContext=React.createContext(45);/* context has 2 things provider and consumer*/
-
+const ValueContext=React.createContext(45,()=>{});/* context has 2 things provider and consumer*/
+//we made a function ()=>{} without anything 
 export default ValueContext;
